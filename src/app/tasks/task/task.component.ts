039791e449f6ca98d2f146467ce09d38c3f6b6ce
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2025-04-11 15:13:35 +0330

    Content Projection 'ng-content'

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { CardComponent } from '../../shared/card/card.component';
 import { TasK } from './task.model';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
