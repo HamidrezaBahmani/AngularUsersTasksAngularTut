a574550661c1d42e68ffcaf740e00d3cfff25fc2
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2025-04-12 19:11:07 +0330

    Bootstrap AngularApp using Module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+import { AppModule } from './app/app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
